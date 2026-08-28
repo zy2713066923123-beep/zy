3c687e77a9df599bd86e9ec8b3893938c19697b8
zhouying <zhouy@zgjzd.cn>
2026-08-28 09:56:26 +0800

    1

--- wxapp/岚财承品.py
+++ wxapp/岚财承品.py
@@ -1,7 +1,8 @@
 import yyb  # 自动同步 yyb_go 存活账号
 
 # name:岚财承品
-# cron:16 10,22 * * *import dataclasses
+# cron:16 10,22 * * *
+import dataclasses
 import os
 import re
 from typing import Any, List, Optional
